03eb73b4be2f82d1e86fd8fb04915c6dfe10645a
Erik Behrends <erik.behrends@gmail.com>
2010-09-07 00:23:27 +0200

    got staging of all questions into generic files working

--- fetch/lama-questions.rb
+++ fetch/lama-questions.rb
@@ -3,9 +3,15 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-Dir.mkdir('questions') unless File.directory?('questions')
+dest_dir = "questionsXYZ"
 
-(1..388).to_a.each do |i|
+Dir.mkdir(dest_dir) unless File.directory?(dest_dir)
+
+q = {}
+
+c = {}
+
+(1..400).to_a.each do |i|
   doc = Nokogiri::HTML(open('http://www.lama-ole-nydahl.de/fragen/?p=' + i.to_s))
 
   qa_section = doc.xpath('//div[@id="inhalt"]/div[@class="article"]')
@@ -23,16 +29,65 @@ Dir.mkdir('questions') unless File.directory?('questions')
 
   categories = qa_section.xpath('.//span[@class="cats"]/a')
 
-  File.open('questions/question' + i.to_s + '.txt', 'w') do |f| 
-    f.write('Question: ' + question + "\n\n") 
-    f.write('Answer: ' + answer + "\n\n") 
+  File.open(dest_dir + '/question' + i.to_s + '.html', 'w') do |f|
+    f.write("<div id='content'>\n")
+    f.write("  <div id='question'>" + question + "</div>\n")
+    f.write("  <div id='answer'>\n")
+    f.write(    answer + "\n")
+    f.write("  </div>\n")
+    f.write("</div>\n")
+  end
 
-    categories.each do |category|
-      f.write('Category: ' + category.content.tr('""','') + "\n\n")
-    end
+  categories.each do |category|
+    cat = category.content.tr('""','')
 
     tags.each do |tag|
-      f.write('Tag: ' + tag.content.tr('""','') + "\n")
+      t = tag.content.tr('""','')
+      c[cat] = c[cat].nil? ? [t] : c[cat].push(t) unless c[cat] && !c[cat].find_index(t).nil?
     end
   end
+
+  tags.each do |tag|
+    t = tag.content.tr('""','')
+    qm = {'question' + i.to_s + '.html' => question}
+    q[t] = q[t].nil? ? [qm] : q[t].push(qm)
+  end
+
+end
+
+i=1
+c.each do |category,tagnames|
+  File.open(dest_dir + '/cat' + i.to_s + '.html', 'w') do |f|
+    f.write("<div id='content'>\n")
+    f.write("  <div id='header'>" + category + "</div>\n")
+    f.write("  <div id='nav'>\n")
+    f.write("    <ul>\n")
+    tagnames.each do |h|
+      f.write("      <li><a href='tag" + (q.keys.find_index(h)+1).to_s  + ".html'>" + h + "</a></li>\n")
+    end
+    f.write("    </ul>\n")
+    f.write("  </div>\n")
+    f.write("</div>\n")
+  end
+  i+=1
+end
+
+i=1
+q.each do |tag,files|
+  File.open(dest_dir + '/tag' + i.to_s + '.html', 'w') do |f|
+    f.write("<div id='content'>\n")
+    f.write("  <div id='header'>" + tag + "</div>\n")
+    f.write("  <div id='nav'>\n")
+    f.write("    <ul>\n")
+    files.each do |h|
+      h.each do |filename,question|
+        f.write("      <li><a href='" + filename + "'>" + question + "</a></li>\n")
+      end
+    end
+    f.write("    </ul>\n")
+    f.write("  </div>\n")
+    f.write("</div>\n")
+  end
+
+  i+=1
 end
