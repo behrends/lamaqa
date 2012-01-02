26b642b448369a684ea09d2b16b2b5a02bc8b5c4
Erik Behrends <erik.behrends@gmail.com>
2012-01-02 01:04:11 +0100

    populate the category hash

--- fetch/fetch.rb
+++ fetch/fetch.rb
@@ -5,8 +5,14 @@ config = YAML.load_file 'config.yml'
 client = XMLRPC::Client.new2 config['url']
 posts = client.call 'metaWeblog.getRecentPosts', 1, config['login'], config['password']
 categories = client.call 'wp.getCategories', 1, config['login'], config['password']
+categories = categories.reduce({}) {|memo, category| memo.merge(Hash[category['categoryName'] => []])}
 
-posts.each do |post|
+posts.sample(8).each do |post|
   puts "#{post['title']} - Keywords: #{post['mt_keywords']}"
   puts post['description']
 end
+
+posts.each { |post| post['categories'].each {|category| categories[category] << post['title'] } }
+
+# this is now a hash of category names mapped to a list of post titles
+puts categories
